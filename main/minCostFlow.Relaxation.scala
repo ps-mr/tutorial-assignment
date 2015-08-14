c24473d08f81ee071096ce8108879eee60675ac4
Yufei Cai <yufei.cai@uni-tuebingen.de>
2015-08-14 14:36:59 +0200

    sort students from last year

--- main/minCostFlow.Relaxation.scala
+++ main/minCostFlow.Relaxation.scala
@@ -18,14 +18,146 @@ object Relaxation {
 
   // test run
   def main(args: Array[String]): Unit = {
+    def now() = java.util.Calendar.getInstance.getTimeInMillis
+    def elapse(last: Long): String = f"${now - last}%8d ms"
+    def report(last: Long, job: String) = println(s"${elapse(last)}  $job")
+
+    def percent(numerator: Int, denominator: Int, thing: String) = {
+      val percent =
+        if (denominator != 0)
+          f"(${Math.round(100.0 * numerator / denominator)}%02d%%)"
+        else
+          s"(--%)"
+      println(f"$numerator%5d $percent%s  $thing")
+    }
+
     args.toList match {
       case List("data") =>
+
+        println()
+
+        val timeInit = now
+
+        val choicePenalty = Seq(0, 3, 6, 5000) // nobody wants to go through hell
+        val groupCapacity = Seq(50, 10, 10, 10)
+        val groupSizeCost = Seq( 0,  2, 15, 45)
+
         val file = new java.io.File(getClass.getResource("").getPath,
           "../../../../data/group-prefs-2014.txt")
 
-        val source = io.Source.fromFile(file)
-        // val students =
-        // run real benchmark
+        val line = io.Source.fromFile(file).getLines()
+        val students = line.next().filter(_.isDigit).toInt
+        val tutors   = line.next().filter(_.isDigit).toInt
+
+        val studentSupply: Supply = Vector.fill(students)(1)
+        val tutorSupply  : Supply = Vector.fill(tutors  )(0)
+        val sortingSupply: Supply = Vector(0, 0, 0, 0) // good, okay, bad, evil
+                                    // or: Gryphindor, Ravenclaw, Hufflepuff, Slytherine
+        val hellSupply   : Supply = Vector(0)
+        val goalSupply   : Supply = Vector(-students)
+        val supply = studentSupply ++ tutorSupply ++ sortingSupply ++ hellSupply ++ goalSupply
+
+        val tutorIndices = Range(students, students + tutors).toVector
+
+        val List(good, okay, bad, evil, hell, goal) = Range(students + tutors, supply.size).toList
+        val sorts = Vector(good, okay, bad, evil)
+        assert(sorts.size == groupSizeCost.size)
+        assert(sorts.size == groupCapacity.size)
+
+        val preferences: IndexedSeq[Seq[Vertex]] =
+          Vector.fill(students) {
+            val prefs = (line.next().split(Array(' ', '\t')).map(_.toInt + students): Seq[Vertex]) :+ hell
+            assert(prefs.size == choicePenalty.size)
+            prefs
+          }
+
+        report(timeInit, "initialization and IO")
+        val timeConstruct = now()
+
+        val prefEdges: Edges = for {
+          (pref, i) <- preferences.zipWithIndex
+          j         <- pref
+        } yield (i, j)
+        val prefCapa: Capacity = prefEdges.map(_ => 1)
+        val prefCost: Cost     = preferences.flatMap(_ => choicePenalty) // includes cost of hell
+
+        val sortEdges: Edges    = for { tutor <- tutorIndices ; sort <- sorts } yield (tutor, sort)
+        val sortCapa : Capacity = tutorIndices.flatMap(_ => groupCapacity)
+        val sortCost : Cost     = tutorIndices.flatMap(_ => groupSizeCost)
+
+        val goalEdges: Edges    = for { i <- Vector(good, okay, bad, evil, hell) } yield (i, goal)
+        val goalCapa : Capacity = goalEdges.map(_ => students)
+        val goalCost : Cost     = goalEdges.map(_ => 0)
+
+        val edges    = prefEdges ++ sortEdges ++ goalEdges
+        val capacity = prefCapa  ++ sortCapa  ++ goalCapa
+        val cost     = prefCost  ++ sortCost  ++ goalCost
+
+        report(timeConstruct, "graph construction")
+
+        val timeCompute = now
+        val flow = computeFlow(supply, edges, cost, capacity)
+        report(timeCompute, "flow computation")
+
+        println()
+
+        val choices = choicePenalty.size
+        assert(choices == 4)
+        def frstChoice(s: Vertex): Edge = choices * s
+        def scndChoice(s: Vertex): Edge = choices * s + 1
+        def thrdChoice(s: Vertex): Edge = choices * s + 2
+        def hellChoice(s: Vertex): Edge = choices * s + 3
+
+        val guys        = Range(0, students)
+        val happy       = guys.count(s => flow(frstChoice(s)) == 1)
+        val sad         = guys.count(s => flow(scndChoice(s)) == 1)
+        val wretched    = guys.count(s => flow(thrdChoice(s)) == 1)
+        val heartbroken = guys.count(s => flow(hellChoice(s)) == 1)
+
+        assert(happy + sad + wretched + heartbroken == students)
+
+        percent(      happy, students, "assigned to 1st choice")
+        percent(        sad, students, "assigned to 2nd choice")
+        percent(   wretched, students, "assigned to 3rd choice")
+        percent(heartbroken, students, "unassigned")
+
+        println()
+
+        val goalEdgeIndices = Range(prefEdges.size + sortEdges.size, edges.size).toList
+        val goalEdgeFlows   =  goalEdgeIndices.map(flow)
+        val List(goodGoal, okayGoal, badGoal, evilGoal, hellGoal) = goalEdgeIndices
+        val List(goodFlow, okayFlow, badFlow, evilFlow, hellFlow) = goalEdgeFlows
+        val goalFlow = goalEdgeFlows.sum
+        assert(goalFlow == students)
+
+        percent(goodFlow, goalFlow, "students in groups of 50 or fewer")
+        percent(okayFlow, goalFlow, "students in groups of size 51--60")
+        percent( badFlow, goalFlow, "students in groups of size 61--70")
+        percent(evilFlow, goalFlow, "students in groups of size 71--80")
+
+        println()
+
+        val groupSizes = tutorIndices.map { tutor =>
+          prefEdges.zipWithIndex.filter({
+            case ((i, j), e) => j == tutor && flow(e) == 1
+          }).size
+        }
+        assert(groupSizes.sum == students - heartbroken)
+
+        val groups     = groupSizes.count(g =>  g != 0)
+        val goodGroups = groupSizes.count(g =>  1 <= g & g <= 50)
+        val okayGroups = groupSizes.count(g => 51 <= g & g <= 60)
+        val  badGroups = groupSizes.count(g => 61 <= g & g <= 70)
+        val evilGroups = groupSizes.count(g => 71 <= g & g <= 80)
+
+        assert(goodGroups + okayGroups + badGroups + evilGroups == groups)
+
+        percent(goodGroups, groups, "groups of 50 or fewer")
+        percent(okayGroups, groups, "groups of size 51--60")
+        percent( badGroups, groups, "groups of size 61--70")
+        percent(evilGroups, groups, "groups of size 71--80")
+
+        println()
 
       case List("random", numberOfVertices, outNeighbs) =>
 
@@ -38,7 +170,6 @@ object Relaxation {
         val penalty       = 2000 // cost for non-assigned vertices
 
         import scala.util.Random
-        def now() = java.util.Calendar.getInstance.getTimeInMillis
         println(s"Generating random graph of $n vertices")
         val startGraphGeneration = now()
 
@@ -196,7 +327,7 @@ object Relaxation {
       if old.capacity(e) > flow(e)
     } yield old.reducedCost(e)
 
-    // alphaCandidates can't be empty
+    // alphaCandidates can be empty. if they are, the instance is infeasible.
     val alpha = alphaCandidates.min
 
     val potential = old.potential.zipWithIndex.map {
