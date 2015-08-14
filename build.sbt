d69f06a6af73b0f2fd5c50a6a796844e8baa8076
Yufei Cai <yufei.cai@uni-tuebingen.de>
2015-08-14 17:28:44 +0200

    rephrase tests as scalatests

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ scalaVersion := "2.11.7"
 scalaSource in Compile := baseDirectory.value / "main"
 
 scalaSource in Test := baseDirectory.value / "test"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
--- main/minCostFlow.Relaxation.scala
+++ main/minCostFlow.Relaxation.scala
@@ -207,51 +207,7 @@ object Relaxation {
         println(f"Flow computed   in ${now - startCompute}%6d ms")
 
       case _ =>
-        // empty instance
-        println("empty vector == " + computeFlow(
-          supply   = Vector.empty,
-          edges    = Vector.empty,
-          cost     = Vector.empty,
-          capacity = Vector.empty
-        ))
-
-        // simple instance
-        println("(10)         == " + computeFlow(
-          supply   = Vector(10, -10),
-          edges    = Vector((0, 1)),
-          cost     = Vector(10),
-          capacity = Vector(10)
-        ))
-
-        // simple 3-vertex instance
-        println("(10, 10)     == " + computeFlow(
-          supply   = Vector(10, 0, -10),
-          edges    = Vector((0, 1), (1, 2)),
-          cost     = Vector(10, 10),
-          capacity = Vector(10, 10)
-        ))
-
-        // 3-vertex instance with choice
-        println("(0, 1, 1)    == " + computeFlow(
-          supply   = Vector(1, 0, -1),
-          edges    = Vector((0, 2), (0, 1), (1, 2)),
-          cost     = Vector(20, 5, 5),
-          capacity = Vector(1, 1, 1)
-        ))
-
-        // http://www.iwr.uni-heidelberg.de/groups/comopt/teaching/ws10/effAlgI/ueb/fernandez_mincostflow.pdf
-        // slide 53: problem instance
-        // slide 56: optimal flow
-        // vertex number = vertex number in slide - 1
-        // edge number   = starting from leftmost vertex: NE, SE;
-        //                     return to leftmost vertex: SE
-        //                      return to topmost vertex: S, NE
-        println("2,0,2,2,4    == " + computeFlow(
-          supply   = Vector(4, 0, 0, -4),
-          edges    = Vector((0,1), (1,3), (0,2), (1,2), (2,3)),
-          cost     = Vector(  2  ,   3  ,   2  ,   1  ,   1  ),
-          capacity = Vector(  4  ,   3  ,   2  ,   2  ,   5  )
-        ))
+        println("invalid usage")
     }
   }
 
