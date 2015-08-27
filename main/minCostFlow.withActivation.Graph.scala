f34592ea081feb85061f783b24d981a7ece7c730
Yufei Cai <yufei.cai@uni-tuebingen.de>
2015-08-27 11:32:25 +0200

    permit arbitrary linear equality constraints in activation flow

--- main/minCostFlow.withActivation.Graph.scala
+++ main/minCostFlow.withActivation.Graph.scala
@@ -1,18 +1,24 @@
 /** PROBLEM Min-cost-flow-with-activation
   *
   * INSTANCE
+  * Matrix A, vector b, set of edges to be activated,
   * Graph where
-  * - every edge has a capacity an activation-cost and a flow-cost
+  * - every edge has a capacity, a binary activation variable, and a cost
   * - every vertex has a supply
   *
   * OBJECTIVE FUNCTION
-  * The sum of flow * flow-cost for every edge,
-  * plus the activation-cost if the flow is nonzero
+  * The sum of flow * cost for every edge,
   *
-  * A min-cost-flow-with-activation instance has all the method
-  * of a min-cost-flow instance, but the former is not a subtype
-  * of the latter, because their objective functions are
-  * different.
+  * CONSTRAINTS
+  * Other than the usual constraints of flow <= capacity * activation
+  * and outgoing-flow - incoming-flow == supply, we have the additional
+  * constraint that
+  *
+  *   A alpha = b,
+  *
+  * where alpha is the activation vector,
+  *           A is the coefficient matrix,
+  *           b is the expected result.
   */
 
 package minCostFlow.withActivation
@@ -23,32 +29,34 @@ import optimus.optimization._
 object Graph {
   import optimus.algebra.{Expression, Const}
 
-  sealed trait Activeness {
-    def toDouble     : Double
-    def toExpression : Expression
-  }
+  case class Matrix(entries: IndexedSeq[IndexedSeq[Int]]) {
+    val rows = entries.length
+    assert(rows != 0)
+    val cols = entries.head.length
+    assert(cols != 0)
+    assert(entries.map(_.length == cols).min)
 
-  case class ActiveVariable(variable: MPIntVar) extends Activeness {
-    def toDouble     : Double     = variable.value.get
-    def toExpression : Expression = variable
-  }
-
-  case object AlwaysActive extends Activeness {
-    def toDouble     : Double     = 1.0
-    def toExpression : Expression = toDouble
+    def * (vector: IndexedSeq[MPIntVar]): IndexedSeq[Expression] = {
+      assert(cols == vector.length)
+      for (row <- entries) yield
+        row.zip(vector).map({ case (c, x) => x * x}).fold[Expression](0)(_ + _)
+    }
   }
 }
 
 trait Graph {
-  val supply         : Supply
-  val edges          : Edges
-  val activationCost : Cost
-  val flowCost       : Cost
-  val capacity       : Capacity
+  val supply   : Supply
+  val edges    : Edges
+  val cost     : Cost
+  val capacity : Capacity
+
+  val activationEdges  : IndexedSeq[Edge]
+  val activationMatrix : Graph.Matrix
+  val activationResult : IndexedSeq[Int]
 
   /** @return (problem, flow, activation)
     */
-  def createIntegerProgram(): (MIProblem, IndexedSeq[MPIntVar], IndexedSeq[Graph.Activeness]) = {
+  def createIntegerProgram(): (MIProblem, IndexedSeq[MPIntVar], IndexedSeq[MPIntVar]) = {
     import optimus.algebra._
     import Graph._
 
@@ -56,16 +64,10 @@ trait Graph {
 
     val edgeIndices = Range(0, edges.length)
 
-    val flow = edgeIndices.map {
-      case i => MPIntVar("flow" + i, Range(0, capacity(i) + 1))
-    }
+    val flow       = edgeIndices    .map { i => MPIntVar("flow" + i, Range(0, capacity(i) + 1)) }
+    val activation = activationEdges.map { i => MPIntVar("activation" + i, 0 to 1) }
 
-    val activation = edgeIndices.map {
-      case i if activationCost(i) == 0 => AlwaysActive
-      case i if activationCost(i) != 0 => ActiveVariable(MPIntVar("activation" + i, 0 to 1))
-    }
-
-    val capacityConstraints = for (i <- edgeIndices) yield flow(i) <= capacity(i) * activation(i).toExpression
+    val capacityConstraints = for (i <- edgeIndices) yield flow(i) <= capacity(i) * activation(i)
 
     val supplyConstraints = for {
       v <- 0 until supply.length
@@ -74,13 +76,14 @@ trait Graph {
     }
     yield outgoingFlow - incomingFlow := supply(v)
 
-    val constraints = capacityConstraints ++ supplyConstraints
-
-    minimize {
-      (  for (i <- edgeIndices)
-         yield flow(i) * flowCost(i) + activation(i).toExpression * activationCost(i)
-      ).fold[Expression](0)(_ + _)
+    assert(activationMatrix.rows == activationResult.length)
+    val activationConstraints = ((activationMatrix * activation), activationResult).zipped.map {
+      case (lhs, rhs) => lhs := rhs
     }
+
+    val constraints = capacityConstraints ++ supplyConstraints ++ activationConstraints
+
+    minimize { (for (i <- edgeIndices) yield flow(i) * cost(i)).fold[Expression](0)(_ + _) }
     subjectTo(constraints: _*)
 
     (problem, flow, activation)
