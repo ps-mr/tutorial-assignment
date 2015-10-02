47ef16b3ace3fa85d931113bdcb67825665172ca
Yufei Cai <yufei.cai@uni-tuebingen.de>
2015-10-02 10:43:44 +0200

    set user fields

--- main/remote.Forum.scala
+++ main/remote.Forum.scala
@@ -18,7 +18,9 @@ object Forum {
     val apiKey       = ("api_key", map("api_key"))
     val dump         = map("dump")
     val setUserField = map("set_user_field")
+    val setUserFields= map("set_user_fields")
 
+    val dataKey      = "data"
     val userid       = "userid"
     val userfield    = "userfield"
     val value        = "value"
@@ -46,20 +48,36 @@ object Forum {
       response.body
   }
 
-  def putRequest(url: String, keyvals: Seq[(String, String)]): HttpRequest =
-    Http(url).postForm(keyvals).method("PUT")
+  implicit class HttpOps(http: HttpRequest) {
+    // must be called last, because HttpRequest.postXYZ set method to POST.
+    def doPut: HttpRequest = http.method("PUT")
+
+    def asJson: HttpRequest = http.header("Content-type", "application/json")
+  }
+
+  def appendApiKey(url: String): String =
+    url + s"?${credential.apiKey._1}=${credential.apiKey._2}"
+
+  def putJson(url: String, json: JsValue): HttpRequest =
+    Http(appendApiKey(url)).postData(json.compactPrint).asJson.doPut
+
+  def setUserFields(fields: Seq[data.Student.Field]): Unit = {
+    val json = Map(credential.dataKey -> fields.map(_.toJson)).toJson
+    val response = putJson(credential.setUserFields, json).asString
+    if (response.isError)
+      sys error response.toString
+  }
 
   def setUserField(userid: Int, key: String, value: String): Unit = {
     val response: HttpResponse[String] =
-      putRequest(
-        credential.setUserField,
+      Http(credential.setUserField).postForm(
         Seq(
           credential.apiKey,
           (credential.userid    , userid.toString),
           (credential.userfield , key),
           (credential.value     , value)
         )
-      ).asString
+      ).doPut.asString
 
     if (response.isError)
       sys error response.toString
