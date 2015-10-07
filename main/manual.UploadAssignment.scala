944b1961a586a93a381d9fcfb6bf35da492713f4
Yufei Cai <yufei.cai@uni-tuebingen.de>
2015-10-07 10:11:38 +0200

    refusing to upload aborts operation
    It doesn't make sense to update display if assignment isn't uploaded.

--- main/manual.UploadAssignment.scala
+++ main/manual.UploadAssignment.scala
@@ -2,11 +2,10 @@ package manual
 
 class UploadAssignment(fields: Seq[data.Student.Field]) extends Process[Unit] {
   def run(): Unit = {
-    println()
-    Process.chooseByUser("Okay to upload?") {
-      reportTime("Uploading") {
-        remote.Forum.setUserFields(fields)
-      }
-    } { () }
+    Process.checkWithUser("Okay to upload?")
+
+    reportTime("Uploading") {
+      remote.Forum.setUserFields(fields)
+    }
   }
 }
